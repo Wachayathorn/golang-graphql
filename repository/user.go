1514c1776776fba238f3e44d94ef6f23a4c7623c
Wachayathorn Singsena <wachayathorn.s@rvconnex.com>
2022-02-22 10:49:04 +0700

    feat : add repository

---
+++ repository/user.go
@@ -0,0 +1,72 @@
+package repository
+
+import (
+	"github.com/wachayathorn/golang-graphql/config"
+	"github.com/wachayathorn/golang-graphql/graph/model"
+)
+
+func CreateUser(input *model.User) (*model.User, error) {
+	tx := config.SQLX.MustBegin()
+	var user model.User
+	err := tx.QueryRow(`INSERT INTO users(firstname, lastname, username) VALUES($1, $2, $3) RETURNING id`, input.Firstname, input.Lastname, input.Username).Scan(&user.ID, &user.Firstname, &user.Lastname, &user.Username)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	tx.Commit()
+	return &user, err
+}
+
+func GetUser() ([]*model.User, error) {
+	var users []*model.User
+	userList, err := config.SQLX.Query(`SELECT * FROM users`)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for userList.Next() {
+		var user model.User
+		err = userList.Scan(&user.ID, &user.Firstname, &user.Lastname, &user.Username)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+	return users, nil
+}
+
+func GetUserById(id int) (*model.User, error) {
+	var user model.User
+	err := config.SQLX.Get(&user, `SELECT * FROM users WHERE id = $1`, id)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func UpdateUser(input *model.User) (*model.User, error) {
+	tx := config.SQLX.MustBegin()
+
+	var user model.User
+	err := tx.QueryRow(`UPDATE users SET firstname = $1, lastname = $2, username = $3 WHERE id = $4`, input.Firstname, input.Lastname, input.Username, input.ID).Scan(&user.ID, &user.Firstname, &user.Lastname, &user.Username)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	tx.Commit()
+	return &user, nil
+}
+
+func DeleteUser(id int) (*model.Response, error) {
+	tx := config.SQLX.MustBegin()
+
+	result, err := tx.Exec(`DELETE FROM users WHERE id = $1`, id)
+	if row, _ := result.RowsAffected(); row == 0 || err != nil {
+		tx.Rollback()
+		return &model.Response{Success: false, Message: "User delete fail"}, err
+	}
+	tx.Commit()
+
+	return &model.Response{Success: true, Message: "User deleted"}, nil
+}
--- service/user.go
+++ service/user.go
@@ -1,73 +1,26 @@
 package service
 
 import (
-	"github.com/wachayathorn/golang-graphql/config"
 	"github.com/wachayathorn/golang-graphql/graph/model"
+	"github.com/wachayathorn/golang-graphql/repository"
 )
 
 func CreateUser(data *model.User) (*model.User, error) {
-	tx := config.SQLX.MustBegin()
-	var id int64
-	err := tx.QueryRow(`INSERT INTO users(firstname, lastname, username) VALUES($1, $2, $3) RETURNING id`, data.Firstname, data.Lastname, data.Username).Scan(&id)
-	if err != nil {
-		tx.Rollback()
-		return nil, err
-	}
-	tx.Commit()
-	user := &model.User{ID: int(id), Firstname: data.Firstname, Lastname: data.Lastname, Username: data.Username}
-	return user, nil
+	return repository.CreateUser(data)
 }
 
 func GetUsers() ([]*model.User, error) {
-	var users []*model.User
-	userList, err := config.SQLX.Query(`SELECT * FROM users`)
-
-	if err != nil {
-		return nil, err
-	}
-
-	for userList.Next() {
-		var user model.User
-		err = userList.Scan(&user.ID, &user.Firstname, &user.Lastname, &user.Username)
-		if err != nil {
-			return nil, err
-		}
-		users = append(users, &user)
-	}
-
-	return users, nil
+	return repository.GetUser()
 }
 
 func GetUserById(id int) (*model.User, error) {
-	var user model.User
-	err := config.SQLX.Get(&user, `SELECT * FROM users WHERE id = $1`, id)
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
+	return repository.GetUserById(id)
 }
 
 func UpdateUser(data *model.User) (*model.User, error) {
-	tx := config.SQLX.MustBegin()
-
-	result, err := tx.Exec(`UPDATE users SET firstname = $1, lastname = $2, username = $3 WHERE id = $4`, data.Firstname, data.Lastname, data.Username, data.ID)
-	if row, _ := result.RowsAffected(); row > 0 && err != nil {
-		tx.Rollback()
-		return nil, err
-	}
-	tx.Commit()
-	return data, nil
+	return repository.UpdateUser(data)
 }
 
 func DeleteUser(id int) (*model.Response, error) {
-	tx := config.SQLX.MustBegin()
-
-	result, err := tx.Exec(`DELETE FROM users WHERE id = $1`, id)
-	if row, _ := result.RowsAffected(); row == 0 || err != nil {
-		tx.Rollback()
-		return &model.Response{Success: false, Message: "User delete fail"}, err
-	}
-	tx.Commit()
-
-	return &model.Response{Success: true, Message: "User deleted"}, nil
+	return repository.DeleteUser(id)
 }
